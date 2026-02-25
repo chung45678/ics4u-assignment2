6ebf19cc7f75402970d5374b9bfedd2b27cfa5ad
chung45678 <hechung128@gmail.com>
2026-02-25 11:56:31 -0500

    Fixed discriminant calculation

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import './style.css'
 
 
 (document.getElementById("solveCubic")as HTMLButtonElement).addEventListener("click", () => {
-  const a = Number(document.getElementById("a-value"))
-  const b = Number(document.getElementById("b-value"))
-  const c = Number(document.getElementById("c-value"))
-  const d = Number(document.getElementById("d-value"))
-  let p = 3 ** a ** c - b^2 / 3 ** a^2
-  let q = 27 ** a^2 ** d
+  const a = Number(document.getElementById("a-value").value)
+  const b = Number(document.getElementById("b-value").value)
+  const c = Number(document.getElementById("c-value").value)
+  const d = Number(document.getElementById("d-value").value)
   
-  const discriminant = (q / 2)^2 + (p/3)^3
+  let p = (3 * a * c - b**2) / (3 * a**2)
+  let q = ((27 * a * a * d) - (9*a*b*c) + (2*b**3)) / (27*a**3)
+
+  console.log(p)
+  console.log(q)
+  
+  const discriminant = (q / 2)**2 + (p/3)**3
   console.log(discriminant)
+
+
   
 })
